56860e66f970009cec03e0460354c898afd1d5c4
BladeWork <gdlt3@126.com>
2014-11-14 05:17:51 +0800

    GIT test

--- mailPilot-v11/Scripts/constants.ts
+++ mailPilot-v11/Scripts/constants.ts
@@ -5,7 +5,7 @@
     export var GAME_OVER_STATE: number = 2;
 
     // Game Constants
-    export var CLOUD_NUM: number = 3;
+    export var CLOUD_NUM: number = 1;
     export var LABEL_FONT = "40px Consolas";
     export var LABEL_COLOUR = "#FFFF00";
     export var PLANE_LIVES = 3;
